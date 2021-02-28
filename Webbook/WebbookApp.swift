75f3912477e75cf74e75ae46e4994a70cc14823b
Lucas <lucasmazza@icloud.com>
2021-02-28 15:17:46 -0600

    Initial Commit

---
+++ Webbook/WebbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebbookApp.swift
+//  Webbook
+//
+//  Created by Lucas Mazza on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct WebbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
